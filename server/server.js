1c58ff7578e31a915f5a9b6170b9989b17a48dd9
SamuelC575 <samuelchoi575@gmail.com>
2026-04-08 23:10:48 -0700

    Refactor server port configuration and logging

--- server/server.js
+++ server/server.js
@@ -59,6 +59,5 @@ io.on('connection', (socket) => {
 });
 
 // Start server
-server.listen(8080, () => {
-    console.log('Server running on http://localhost:8080');
-});
+const PORT = process.env.PORT || 8080;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
