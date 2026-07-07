7684be6637e22f71835df9f6532f9ad5024d87f0
Samuel Choi <samuelchoi575@gmail.com>
2026-07-07 11:05:54 -0700

    r we back? nah pasting old code 4 fun

--- server/server.js
+++ server/server.js
@@ -160,9 +160,7 @@ io.on('connection', (socket) => {
     }
 });
 
-// ----------------------
-// START SERVER
-// ----------------------
+// server start
 const PORT = process.env.PORT || 8080;
 server.listen(PORT, () => {
     // console.log(`Server running on port ${PORT}`);
